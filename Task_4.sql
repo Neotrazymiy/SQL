709e646f6a48bb876279784bb3ee1adc549b4761
Admin <Admin@DESKTOP-GGK0IBF>
2025-01-27 14:04:19 +0200

    Task_10

---
+++ Task_4.sql
@@ -0,0 +1,6 @@
+--Найдите все записи таблицы Printer для цветных принтеров.
+
+SELECT
+  *
+FROM Printer
+WHERE color = 'y';
---
+++ Task_6.sql
@@ -0,0 +1,19 @@
+--Для каждого производителя, выпускающего ПК-блокноты c объёмом 
+--жесткого диска не менее 10 Гбайт, найти скорости таких ПК-блокнотов. 
+--Вывод: производитель, скорость.
+
+SELECT DISTINCT
+    (
+        SELECT 
+            maker
+        FROM 
+            Product
+        WHERE 
+            Product.model = Laptop.model
+            AND Product.type = 'Laptop'
+    ) AS maker,
+    speed
+FROM 
+    Laptop
+WHERE 
+    hd >= 10;
